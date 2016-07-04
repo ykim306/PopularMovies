5070dae2ba1faa4e60323e643a7e5033c22e0de8
ykim306 <ykim306@gmail.com>
2016-07-04 12:02:27 -0400

    Added Movies Fragment
    Removed all Layout to new fragment and added code to communicate between
    main activity and fragment

--- app/src/main/java/app/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/app/com/example/android/popularmovies/MainActivity.java
@@ -1,13 +1,31 @@
 package app.com.example.android.popularmovies;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements MoviesFragment.OnFragmentInteractionListener {
+
+    // TODO: Add Log TAG for debug
+    private static final String TAG_NAME = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // TODO: Add fragment initiation
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+            // TODO: Call to the MoviesFragment code
+            .add(R.id.movies_container, new MoviesFragment())
+            .commit();
+        }
     }
+
+    // TODO: Be sure to add this to manage fragment event
+    @Override
+    public void onFragmentInteraction(Uri uri) { return; }
+
 }
